f87af3a34433d5caf1ad02baa6ea4dea80c11af0
Nestor Gutierrez <nestorg456k@outlook.com>
2025-09-12 11:24:53 -0500

    refactor: eliminar sistema completo de POIs del mapa
    - Remover componentes de mapa: MapHeader, PoiDetailSheet, SearchResultsList
    - Eliminar servicios y stores relacionados: mapService, mapStore
    - Remover datos mock de POIs
    - Eliminar componente PullToRefresh de animaciones

--- ponti-frontend/src/services/mapService.ts
+++
@@ -1,9 +0,0 @@
-import { mockPois } from "@/data/mockPois";
-import { PoiItem } from "@/data/types";
-
-export async function fetchPois(): Promise<PoiItem[]> {
-  await new Promise((r) => setTimeout(r, 400));
-  return mockPois;
-}
-
-
