ba5d04c9f9dd23e46b4101d40a835ce0e6e8c31d
Nestor Gutierrez <nestorg456k@outlook.com>
2025-09-18 11:12:07 -0500

    chore: Eliminación completa de la implementación de la mock API de Ponti
    - Se removieron todos los archivos relacionados con ponti-api del proyecto

--- ponti-api/src/models/index.ts
+++
@@ -1,7 +0,0 @@
-export { Student, IStudent } from './Student';
-export { Campus, ICampus } from './Campus';
-export { Program, IProgram } from './Program';
-export { Course, ICourse } from './Course';
-export { Schedule, ISchedule } from './Schedule';
-export { Grades, IGrades } from './Grades';
-export { Announcements, IAnnouncements } from './Announcements';
--- ponti-api/src/repositories/index.ts
+++
@@ -1,4 +0,0 @@
-export { StudentRepository, IStudentRepository } from './StudentRepository';
-export { ScheduleRepository, IScheduleRepository } from './ScheduleRepository';
-export { AnnouncementRepository, IAnnouncementRepository, AnnouncementFilters, PaginationOptions, PaginatedResult } from './AnnouncementRepository';
-export { CampusRepository, ICampusRepository } from './CampusRepository';
